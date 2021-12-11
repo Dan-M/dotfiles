eeb493d3522f55ff89f54da333f8d768cc395ec7
Dan Maffiew <dmaffiew@gmail.com>
2021-12-11 12:36:23 +0700

    neuron which key

--- .config/lvim/lua/user/neuron.lua
+++ .config/lvim/lua/user/neuron.lua
@@ -11,7 +11,7 @@ M.config = function()
     mappings = true,
     run = nil,
     neuron_dir = "~/neuron",
-    leader = "gz",
+    leader = "<leder>gz",
   }
 end
 
@@ -21,10 +21,12 @@ lvim.builtin.which_key.mappings["gz"] = {
 --  ] = { "require'neuron'.goto_prev_extmark()", "Previous Ext Mark" },
 --  [ = { "require'neuron'.goto_next_extmark()", "Next Ext Mark" },
 --  <CR> = { "require'neuron'.enter_link(), Show linked note" },
-  B = { "<cmd>lua require'neuron/telescope'.find_backlink()<cr>", "Find Backlinks" },
+  b = { "<cmd>lua require'neuron/telescope'.find_backlink()<cr>", "Find Backlinks" },
+  B = { "<cmd>lua require'neuron/telescope'.find_backlink() { insert = true }<cr>", "Find Backlink & insert" },
   s = { "<cmd>lua require'neuron'.rib()<cr>", "Serve" },
   t = { "<cmd>lua require'neuron/telescope'.find_tags()<cr>", "Find Tags" },
   z = { "<cmd>lua require'neuron/telescope'.find_zettels()<cr>", "Find Zettels" },
+  Z = { "<cmd>lua require'neuron/telescope'.find_zettels() { insert = true }<cr>", "Find Zettel & insert link" },
 
 }
 return M
