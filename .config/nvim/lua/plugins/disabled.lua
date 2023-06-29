ce9b8b6256c0dc177ad32fca713f8c2e68d28c76
Dan Maffiew <dmaffiew@gmail.com>
2023-06-29 13:19:55 +0700

    disabled plugins

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/neodev.nvim", enabled = false },
+  { "folke/neoconf.nvim", enabled = false },
+  { "gorbit99/codewindow.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+}
