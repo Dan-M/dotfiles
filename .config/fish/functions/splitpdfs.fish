0fb1e46e33dcd2aaba41893e5829e669731c7dbb
Dan Maffiew <dmaffiew@gmail.com>
2024-07-16 12:20:05 +0700

    split pdfs return

--- .config/fish/functions/splitpdfs.fish
+++ .config/fish/functions/splitpdfs.fish
@@ -1,11 +1,11 @@
 function splitpdfs -d "Split all pdfs in a folder into single page pdfs" -a path
     if not type --quiet qpdf
         echo "splitpfds requires qpdf to work"
-        exit 1
+        return
     end
     if not test -d $path
         echo "Usage: splitpds ./path/to/dir"
-        exit 2
+        return
     end
     for file in $path/*.pdf
         set pageName $(string replace '.pdf' '_%d.pdf' $file)
